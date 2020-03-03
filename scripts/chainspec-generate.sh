aa643448ee6ed6bec06a021cec2907e79db4f357
Andreas Krueger <coding@AndreasKrueger.de>
2020-03-03 19:42:06 +0100

    version v0.9 bump

--- scripts/chainspec-generate.sh
+++ scripts/chainspec-generate.sh
@@ -1,7 +1,7 @@
 # for 4 nodes start this:
 #                        chainspec-generate.sh 4
 #
-# version 0.8
+# version 0.9
 # https://github.com/drandreaskrueger/substrate-starter
 
 # TODO: remove the bootnode ?
